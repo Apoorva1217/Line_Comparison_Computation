13f20abc073f810b76573fa9e8acd8357ea9f398
Apoorva1217 <Apoorva1217@gmail.com>
2020-10-22 16:08:53 +0530

    [Apoorva] Add . Welcome Message to Line Comparison Computation Program

---
+++ Line_Comparison_Computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparison_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
